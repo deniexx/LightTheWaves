bc074645fa116623906a809698e5c201d05f823b
Denis Dimitrov <deniexxx17@gmail.com>
2024-02-08 17:41:19 +0000

    Boss first proof of concept, spawns and can be killed, does not attack yet

---
+++ Source/LightTheWaves/Public/Interface/CBWaveDirector.h
@@ -0,0 +1,74 @@
+// Made By Cubic Burrito
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "CBWaveDirector.generated.h"
+
+UENUM(BlueprintType)
+enum class EActivityFinishedState
+{
+	Successful,
+	Failed,
+	Ongoing,
+
+	MAX UMETA(Hidden)
+};
+
+UENUM(BlueprintType)
+enum class EGameActivity
+{
+	Wave,
+	Recess,
+	Boss,
+	Unknown,
+
+	// No previous Activity
+	None,
+
+	MAX UMETA(Hidden)
+	
+};
+
+USTRUCT(BlueprintType)
+struct FActivityStateUpdatedData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly)
+	EActivityFinishedState ActivityFinishedState;
+
+	UPROPERTY(BlueprintReadOnly)
+	EGameActivity OldActivity;
+
+	UPROPERTY(BlueprintReadOnly)
+	EGameActivity NewActivity;
+	
+	/** @NOTE: Add any additional data that needs to be sent here */
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnActivityStateUpdated, FActivityStateUpdatedData, ActivityStateUpdatedData);
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UCBWaveDirector : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class LIGHTTHEWAVES_API ICBWaveDirector
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Wave Director")
+	void StartWaveGameplay();
+	
+	virtual FOnActivityStateUpdated& OnActivityStateUpdatedEvent() = 0;
+};
